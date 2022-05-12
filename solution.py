68756a565a408eccb6c1baf2b27b1562647ae952
youngizere <Young.Izere@strathmore.edu>
2022-05-12 17:02:14 +0300

    Solution refinement

--- solution.py
+++ solution.py
@@ -1,5 +1,9 @@
 def min_max_sum(input_list):
     input_list.sort()
+    # Given that the input list/array is now sorted, the minimum sum is 
+    # the sum of the first 4 elements
+    # 
+    # The maximum sum is the sum of the last 4 elements in the list/array 
     minimum_sum = sum(input_list[:4])
     maximum_sum = sum(input_list[1:])
     return print(f'{minimum_sum} {maximum_sum}')
